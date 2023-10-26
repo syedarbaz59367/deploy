128925a2ba821df8f659bd88a03c0558b70dfcfe
Syed Arbaz <79559252+syedarbaz59367@users.noreply.github.com>
2023-10-26 02:12:44 -0400

    server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use("/api/v1/product", productRoutes);
 //   res.send("<h1>Welcome to ecommerce mern app</h1>");
 // });
 
-app.use("*", function (req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
 
